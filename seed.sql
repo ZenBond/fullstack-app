a6ccdf4eaded78e2a34414a1416d860edc9945aa
Zoi <zoilanbond@gmail.com>
2023-11-13 13:29:33 -0800

    seed file created and generated

---
+++ seed.sql
@@ -0,0 +1,21 @@
+INSERT INTO team (city, name) VALUES ('Los Angeles', 'Lakers');
+INSERT INTO team (city, name) VALUES ('Miami', 'Heat');
+INSERT INTO team (city, name) VALUES ('Dallas', 'Mavericks');
+
+INSERT INTO player (name, age, team_id) VALUES ('Dangelo Russel', 27, 1);
+INSERT INTO player (name, age, team_id) VALUES ('Cam Reddish', 24, 1);
+INSERT INTO player (name, age, team_id) VALUES ('Lebron James', 38, 1);
+INSERT INTO player (name, age, team_id) VALUES ('Jarred Vanderbilt', 24, 1);
+INSERT INTO player (name, age, team_id) VALUES ('Anthony Davis', 30, 1);
+INSERT INTO player (name, age, team_id) VALUES ('Kyle Lowry', 37, 2);
+INSERT INTO player (name, age, team_id) VALUES ('Tyler Herro', 23, 2);
+INSERT INTO player (name, age, team_id) VALUES ('Jimmy Butler', 34, 2);
+INSERT INTO player (name, age, team_id) VALUES ('Haywood Highsmith', 26, 2);
+INSERT INTO player (name, age, team_id) VALUES ('Bam Adebayo', 26, 2);
+INSERT INTO player (name, age, team_id) VALUES ('Luka Doncic', 24, 3);
+INSERT INTO player (name, age, team_id) VALUES ('Kyrie Irving', 31, 3);
+INSERT INTO player (name, age, team_id) VALUES ('Derrick Jones', 26, 3);
+INSERT INTO player (name, age, team_id) VALUES ('Grant Williams', 24, 3);
+INSERT INTO player (name, age, team_id) VALUES ('Dereck Lively', 19, 3);
+
+
